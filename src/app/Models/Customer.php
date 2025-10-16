85b8fd97e6249c1a1ed2ed09584810b9f651006f
Matias Hernan Lauriti <matiaslauriti@gmail.com>
2025-10-16 22:00:15 +0200

    Add Laravel IDE helper

--- src/app/Models/Customer.php
+++ src/app/Models/Customer.php
@@ -5,6 +5,53 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $scan_id
+ * @property int $external_customer_id
+ * @property int $fraudulent
+ * @property int $bsn
+ * @property string $iban
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $date_of_birth
+ * @property string $phone_number
+ * @property string $street
+ * @property string $city
+ * @property string $postal_code
+ * @property array<array-key, mixed> $products
+ * @property string $tag
+ * @property string $ip_address
+ * @property \Illuminate\Support\Carbon|null $last_invoice_at
+ * @property \Illuminate\Support\Carbon|null $last_login_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Scan $scan
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereBsn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereDateOfBirth($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereExternalCustomerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereFraudulent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereIban($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereIpAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereLastInvoiceAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer wherePostalCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereProducts($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereScanId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereStreet($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereTag($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Customer extends Model
 {
     protected $fillable = [
--- src/app/Models/Scan.php
+++ src/app/Models/Scan.php
@@ -5,6 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Customer> $customers
+ * @property-read int|null $customers_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Scan extends Model
 {
     public function customers(): HasMany
