06b88a03c1e1e3463f9291343252a7c6f3ec170e
Matias Hernan Lauriti <matiaslauriti@gmail.com>
2025-10-19 23:11:15 +0200

    Fix trying to read possible empty object

--- src/resources/js/pages/Scan/Index.vue
+++ src/resources/js/pages/Scan/Index.vue
@@ -5,7 +5,7 @@ import Scan from "@/components/Scan.vue";
 import { Scan as ScanType } from "@/types";
 
 const { scans } = defineProps<{
-    scans: {
+    scans?: {
         data: ScanType[];
         meta: any;
     },
@@ -16,7 +16,7 @@ const { scans } = defineProps<{
     <Head title="Scan Index" />
     <h2 class="text-2xl text-center mb-6">Existing Scans</h2>
 
-    <div class="border rounded p-2 mx-auto my-4" v-for="scan in scans.data">
+    <div class="border rounded p-2 mx-auto my-4" v-for="scan in scans?.data">
         <Scan :scan="scan" />
     </div>
 
