51146e1049e1b837c8084c21309175bb88304692
Matias Hernan Lauriti <matiaslauriti@gmail.com>
2025-10-19 17:35:30 +0200

    Add status column to the Scan's model

--- src/app/Models/Scan.php
+++ src/app/Models/Scan.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Domain\Enums\ScanStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property int $id
+ * @property ScanStatus $status
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Customer> $customers
@@ -16,11 +18,20 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan query()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan whereStatus($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Scan whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Scan extends Model
 {
+    protected $fillable = [
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => ScanStatus::class,
+    ];
+
     public function customers(): HasMany
     {
         return $this->hasMany(Customer::class);
